0800034fdc515fe6c5172c4156b581be0b866bdd
Kwasniok <Kwasniok@users.noreply.github.com>
2022-10-23 17:12:16 +0200

    add namespace doc

---
+++ include/gttl/namespaces_doc.hpp
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * @brief This file is for documentation of namespaces only.
+ */
+
+/**
+ * @brief namespace for entire generic tensor template libraray
+ */
+namespace gttl
+{
+} // namespace gttl
