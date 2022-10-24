6f84315682054c4a3f6fa3825d05966f6137a5df
Kwasniok <Kwasniok@users.noreply.github.com>
2022-10-24 21:56:51 +0200

    add top level header

---
+++ include/gttl.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @brief top level header file
+ */
+
+#ifndef GTTL_HPP
+#define GTTL_HPP
+
+#include "gttl/dimensions.hpp"
+#include "gttl/index.hpp"
+#include "gttl/multi_index.hpp"
+#include "gttl/multi_index_range.hpp"
+
+#endif
