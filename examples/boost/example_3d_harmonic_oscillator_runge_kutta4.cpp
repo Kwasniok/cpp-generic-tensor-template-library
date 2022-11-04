53bd6e2a6db92dcdfe2e2e4e717f11b106c4d31a
Kwasniok <Kwasniok@users.noreply.github.com>
2022-11-04 19:39:12 +0100

    add example

---
+++ examples/boost/example_3d_harmonic_oscillator_runge_kutta4.cpp
@@ -0,0 +1,49 @@
+// clang-format off
+#include <iostream>
+
+#include <boost/numeric/odeint/integrate/integrate_const.hpp>
+#include <boost/numeric/odeint/stepper/runge_kutta4.hpp>
+
+#include <gttl.hpp>
+#include <gttl/extensions/boost/numeric/odeint.hpp>
+
+using namespace gttl::literals; // for _D
+
+using Scalar = double;
+
+template <std::size_t N>
+using Dims = gttl::Dimensions<N>;
+
+using Vec = gttl::Tensor<Scalar, 1, Dims<1>{3_D}>;
+using PhaseVec = gttl::Tensor<Scalar, 1, Dims<1>{6_D}>;
+
+constexpr Vec k{0.1, 0.2, 0.3};
+constexpr Vec gam{0.25, 0.15,0.25};
+
+void harmonic_oscillator(const PhaseVec& p, PhaseVec& dpdt, const Scalar /* t */)
+{
+    constexpr std::size_t DIM {3};
+
+    for (std::size_t i{0}; i < DIM; ++i) {
+        // dxdt = v
+        dpdt[0 + i] = p[DIM + i];
+        // dvdt = -k * x -2 * gamma * v
+        dpdt[DIM + i] = -k[i] * p[0 + i] - 2 * gam[i] * p[DIM + i];
+    }
+}
+
+int main()
+{
+    using namespace boost::numeric::odeint;
+    using std::cout;
+
+    PhaseVec phase = {1, 1, 1, 0, 0, 0};
+
+    cout << "phase(t=0):  ";
+    cout << phase << '\n';
+    runge_kutta4<PhaseVec, Scalar> stepper;
+    integrate_const(stepper, harmonic_oscillator, phase, 0.0, 10.0, 0.01);
+
+    cout << "phase(t=10): ";
+    cout << phase << '\n';
+}
